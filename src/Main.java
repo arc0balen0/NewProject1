cf6a0bab9cc8e82f882878d959cd084dc5cd33ed
Pavel Khudenko <100493400+arc0balen0@users.noreply.github.com>
2023-05-10 23:24:39 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,27 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Person> persons = new ArrayList<>();
-        persons.add(new Person("Иван", "Петров", 25));
-        persons.add(new Person("Сергей", "Кузнецов", 30));
-        persons.add(new Person("Елена", "Петрова-Сидорова", 28));
-        persons.add(new Person("Алексей", "Сидоров", 35));
+        persons.add(new Person("Иван", "Иванов", 35));
+        persons.add(new Person("Петр", "Петрович Иванов", 25));
+        persons.add(new Person("Сергей", "Сергеевич Сергеев", 45));
 
-        Collections.sort(persons, new PersonComparator());
+        Collections.sort(persons, (p1, p2) -> {
+            String[] p1SurnameWords = p1.getSurname().split(" ");
+            String[] p2SurnameWords = p2.getSurname().split(" ");
+
+            int p1SurnameWordCount = p1SurnameWords.length;
+            int p2SurnameWordCount = p2SurnameWords.length;
+
+            if (p1SurnameWordCount != p2SurnameWordCount) {
+                return Integer.compare(p2SurnameWordCount, p1SurnameWordCount);
+            } else {
+                return Integer.compare(p2.getAge(), p1.getAge());
+            }
+        });
 
         for (Person person : persons) {
             System.out.println(person);
         }
     }
 }
+
